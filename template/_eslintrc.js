6570c1c96a7eef815de11ce12b759d36ef2a79b9
Brent Kelly <brentkelly87@gmail.com>
2020-09-03 19:01:38 +0100

    :children_crossing: Adding useful yarn scripts, auto lint/prettier, import aliases, stricter TS and VS Code configuration

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -3,4 +3,32 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    // Prettier
+    'prettier/prettier': [1, {
+      "singleQuote": true,
+      "trailingComma": "es5",
+      "jsxBracketSameLine": true,
+      "parser": "typescript",
+      "tabWidth": 2,
+      "printWidth": 100
+    }],
+
+    // ESLint
+    'no-undef': 0, // Conflicts with TS
+
+    // TS
+    '@typescript-eslint/explicit-member-accessibility': 1,
+    '@typescript-eslint/explicit-function-return-type': [1, {
+      allowExpressions: true,
+      allowTypedFunctionExpressions: false,
+      allowHigherOrderFunctions: false,
+    }],
+    '@typescript-eslint/no-use-before-define': [2, {
+      "functions": false,
+    }],
+
+    // React
+    'react/jsx-boolean-value': [2, 'always'],
+  }
 };
--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.json'],
+        alias: {
+          '@components': './app/components',
+        },
+      },
+    ],
+  ],
 };
