b2d4641e9f94ce5827a1f51abd9e621ea530167c
Brent Kelly <brentkelly87@gmail.com>
2021-04-05 18:14:25 +0100

    :recycle: Using eslint-config-react-native-bk

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,27 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    // Prettier
-    'prettier/prettier': 1,
-
-    // ESLint
-    'comma-dangle': 0, // Conflicts with prettier
-    'no-undef': 0, // Conflicts with TS
-
-    // TS
-    '@typescript-eslint/ban-ts-comment': 1,
-    '@typescript-eslint/no-inferrable-types': 1,
-    '@typescript-eslint/explicit-member-accessibility': 1,
-    '@typescript-eslint/explicit-function-return-type': [
-      1,
-      {
-        allowExpressions: true,
-      },
-    ],
-
-    // React
-    'react/function-component-definition': 1,
-    'react/jsx-boolean-value': [2, 'always'],
-  },
+  extends: ['react-native-bk'],
 };
